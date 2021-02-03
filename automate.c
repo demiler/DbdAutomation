839b8f5fafca9aa0ca921052f8ae732220b92c19
demiler <seerpout@gmail.com>
2021-02-03 22:26:55 +0300

    toxic action added

--- automate.c
+++ automate.c
@@ -38,10 +38,10 @@ void wiggle(void) {
   printf("Wiggle script disabled\n");
 }
 
-//_ 1) printf, beep
-//_ 2) run for n seconds then warning 
-//_ 3) run for n + p seconds then disable
-//_ 4) press 'space' button while running
+//X 1) printf, beep
+//X 2) run for n seconds then warning 
+//X 3) run for n + p seconds then disable
+//X 4) press 'space' button while running
 //_ 5) reset by middle mouse button
 //_ 6) stop by 'shift' or 'ctrl'
 void struggle(void) {
@@ -67,4 +67,43 @@ void struggle(void) {
 
   makeSound(S_SCRIPT_DISABLED);
   printf("Struggle script disabled\n");
+}
+
+//1)_ printf
+//2)_ disable if shift pressed
+//3)_ detect if lmb is pressed
+//4)_ press ctrl or lmb while running
+//5)_ run until ctrl or lmb is pressed
+//    run at least for few sec if neither 
+//    pressed.
+void becomeToxic(void) {
+  printf("Become toxic script active\n");
+
+  const time_t stopTime = 500;
+  const time_t tBagTime = 60;
+  const time_t clickTime = 30;
+  const bool tBag = true;
+
+  if (tBag)
+    pressKey(KBK_CTRL, 125);
+
+  time_t runTime = 0, startTime = millis();
+  while (runTime < stopTime) {
+    if (tBag) {
+      pushKey(KBK_CTRL);
+      Sleep(tBagTime);
+      releaseKey(KBK_CTRL);
+      Sleep(tBagTime);
+    }
+    else {
+      pushMouseBtn(MSK_RIGHT);
+      Sleep(clickTime);
+      releaseMouseBtn(MSK_RIGHT);
+      Sleep(clickTime);
+    }
+
+    runTime = millis() - startTime;
+  }
+
+  printf("Become toxic script disable\n");
 }
